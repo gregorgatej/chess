90503c5a6c6929ef18b3aebc40e85431fc552c7a
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-17 15:22:44 +0100

    feat: integrate option to play vs ComputerPlayer in Game

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -2,7 +2,7 @@ module Chess
   class Game
     SAVED_GAME_PATH = "./data/saved_game.json"
 
-    attr_reader :board, :player_white, :player_black, :current_player
+    attr_reader :board, :player_white, :player_black, :current_player, :game_type
 
     def initialize
       @board = Board.new
@@ -10,12 +10,16 @@ module Chess
       @player_black = Player.new(:black)
       @current_player = player_white
       @skip_save_prompt = false
+      @game_type = nil
     end
 
     def save_game
       f = File.new SAVED_GAME_PATH, "w+"
       data = board.to_h
       data["current_player"] = current_player.to_s
+      data["game_type"] = game_type
+      data["player_white_type"] = player_white.is_a?(ComputerPlayer) ? "computer" : "human"
+      data["player_black_type"] = player_black.is_a?(ComputerPlayer) ? "computer" : "human"
       f.write(JSON.dump(data))
       f.close
       puts "Successfully written data to disk."
@@ -24,44 +28,69 @@ module Chess
     def load_game
       data = JSON.load(File.read(SAVED_GAME_PATH))
       @board = Board.from_h(data)
-      @player_white = Player.new(:white)
-      @player_black = Player.new(:black)
+      @game_type = data["game_type"]
+
+      white_type = data["player_white_type"]
+      black_type = data["player_black_type"]
+      @player_white = white_type == "computer" ? ComputerPlayer.new(:white) : Player.new(:white)
+      @player_black = black_type == "computer" ? ComputerPlayer.new(:black) : Player.new(:black)
+
       @current_player = data["current_player"] == player_white.to_s ? player_white : player_black
       @skip_save_prompt = true
       puts "Game successfully loaded.\n"
     end
 
     def play
-      puts "Welcome to the game of chess!"
+      puts "Welcome to the game of Chess!"
       if File.exist? SAVED_GAME_PATH
         puts "Do you want to load previously saved game? (y/n)" 
         load_game if gets.chomp.downcase == "y"
       end
 
+      unless @game_type
+        choose_game_type
+      end
+
       loop do
         puts "\n#{board}"
         puts "#{current_player}'s turn"
-        puts "Enter move (e.g., '4,6 4,4' for col,row from-to) or 'exit' to quit:"
 
-        input = gets.chomp
-        break if input == "exit"
-        
-        input = input.split
-        unless valid_input?(input)
-          puts "Invalid input!"
-          next
-        end
+        if current_player.is_a?(ComputerPlayer)
+          puts "Computer is thinking..."
+          sleep(1)
+          move = current_player.make_move(board)
+          
+          if move.nil?
+            puts "Computer has no legal moves!"
+            break
+          end
 
-        from = input[0].split(",").map(&:to_i)
-        to = input[1].split(",").map(&:to_i)
+          from = move[:from]
+          to = move[:to]
+          puts "Computer moves: #{from} to #{to}"
+        else
+          puts "Enter move (e.g., '4,6 4,4' for col,row from-to) or 'exit' to quit:"
 
-        # Convert from col,row to row,col for internal use
-        from = [from[1], from[0]]
-        to = [to[1], to[0]]
+          input = gets.chomp
+          break if input == "exit"
+          
+          input = input.split
+          unless valid_input?(input)
+            puts "Invalid input!"
+            next
+          end
 
-        unless board.valid_move?(from, to, current_player)
-          puts "Invalid move!"
-          next
+          from = input[0].split(",").map(&:to_i)
+          to = input[1].split(",").map(&:to_i)
+
+          # Convert from col,row to row,col for internal use
+          from = [from[1], from[0]]
+          to = [to[1], to[0]]
+
+          unless board.valid_move?(from, to, current_player)
+            puts "Invalid move!"
+            next
+          end
         end
 
         if board.move_leaves_king_in_check?(from, to, current_player)
@@ -95,7 +124,32 @@ module Chess
     private
 
     attr_reader :skip_save_prompt
-    attr_writer :current_player
+
+    def choose_game_type
+      puts "\nChoose game mode:"
+      puts "1. Human vs Human"
+      puts "2. Human vs Computer"
+      puts "Enter your choice (1 or 2):"
+      
+      choice = gets.chomp
+      
+      case choice
+      when "1"
+        @game_type = "human_vs_human"
+      when "2"
+        @game_type = "human_vs_computer"
+        puts "Do you want to play as White? (y/n)"
+        if gets.chomp.downcase == "y"
+          @player_black = ComputerPlayer.new(:black)
+        else
+          @player_white = ComputerPlayer.new(:white)
+          @current_player = player_white
+        end
+      else
+        puts "Invalid choice. Defaulting to Human vs Human."
+        @game_type = "human_vs_human"
+      end
+    end
 
     def valid_input?(input)
       input.length == 2 && input.all? { |pos| pos.split(",").length == 2 }
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,17 +14,42 @@ describe Chess::Game do
       expect(game.player_white.color).to eq(:white)
       expect(game.player_black.color).to eq(:black)
     end
+
+    it "initializes game_type as nil" do
+      expect(game.game_type).to be_nil
+    end
   end
 
   describe "#play" do
+    before do
+      allow(File).to receive(:exist?).and_return(false)
+    end
     it "calls move_piece on the board when a valid move is made" do
-      allow(game).to receive(:gets).and_return("4,6 4,4\n", "exit\n")
+      allow(game).to receive(:gets).and_return("1\n", "4,6 4,4\n", "exit\n")
       allow(game).to receive(:puts)
       allow(game.board).to receive(:valid_move?).and_return(true)
 
       expect(game.board).to receive(:move_piece).with([6, 4], [4, 4])
-    
       game.play
+    end
+
+    it "selects human_vs_human game type when player chooses 1" do
+      allow(game).to receive(:gets).and_return("1\n", "exit\n")
+      allow(game).to receive(:puts)
+      allow(game.board).to receive(:valid_move?).and_return(true)
+
+      game.play
+      expect(game.game_type).to eq("human_vs_human")
+    end
+
+    it "selects human_vs_computer with player as white when player chooses 2 then y" do
+      allow(game).to receive(:gets).and_return("2\n", "y\n", "exit\n")
+      allow(game).to receive(:puts)
+      allow(game.board).to receive(:valid_move?).and_return(true)
+
+      game.play
+      expect(game.game_type).to eq("human_vs_computer")
+      expect(game.player_black).to be_a(Chess::ComputerPlayer)
     end
   end
 
@@ -56,7 +81,7 @@ describe Chess::Game do
       game = Chess::Game.new
       allow(game).to receive(:puts)
       # Simulate switching to black player
-      game.send(:current_player=, game.player_black)
+      game.instance_variable_set(:@current_player, game.player_black)
       game.save_game
       
       new_game = Chess::Game.new
@@ -65,21 +90,28 @@ describe Chess::Game do
       expect(new_game.current_player.to_s).to eq("Black Player")
     end
 
-    it "restores the full game state including board and current player" do
+    it "restores the full game state including board, current player, game_type, and player types" do
       allow(game).to receive(:puts)
 
       game = Chess::Game.new
       allow(game).to receive(:puts)
       game.board.move_piece([6, 0], [4, 0])
-      game.send(:current_player=, game.player_black)
+      game.instance_variable_set(:@current_player, game.player_black)
+      game.instance_variable_set(:@game_type, "human_vs_computer")
+      game.instance_variable_set(:@player_black, Chess::ComputerPlayer.new(:black))
       game.save_game
 
       new_game = Chess::Game.new
       allow(new_game).to receive(:puts)
       new_game.load_game
+
       expect(new_game.board.state[4][0]).to be_a(Chess::Pawn)
       expect(new_game.board.state[6][0]).to be_nil
       expect(new_game.current_player.to_s).to eq("Black Player")
+      expect(new_game.game_type).to eq("human_vs_computer")
+      expect(new_game.player_white).to be_a(Chess::Player)
+      expect(new_game.player_white).not_to be_a(Chess::ComputerPlayer)
+      expect(new_game.player_black).to be_a(Chess::ComputerPlayer)
     end
   end
 end
